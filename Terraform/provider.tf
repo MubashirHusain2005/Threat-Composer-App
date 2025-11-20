3313d5ee80e41126bb76ddefd5ec84ecb7233cce
MubashirHusain2005 <stokemubashir@gmail.com>
2025-11-20 18:24:32 +0000

    Final Pipeline

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -11,6 +11,43 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+terraform {
+  backend "S3" {
+    bucket   = "mubashir-tf-state-bucket"
+    key      = "terraform.tfstate"
+    region   = "terraform-locks"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.20.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "mubashir-tf-state-bucket"
+
+    tags = {
+      Name = "tf-S3"
+      Environment = "DEV"
+    }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 
 
 
