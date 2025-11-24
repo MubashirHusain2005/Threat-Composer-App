1ba1e001c02d33049973d4ee557d25e7f0cf1d66
MubashirHusain2005 <stokemubashir@gmail.com>
2025-11-24 11:53:51 +0000

    Updated Security

--- Terraform/Modules/SG/main.tf
+++ Terraform/Modules/SG/main.tf
@@ -21,17 +21,11 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = var.cidr_block
   }
 
-  ingress {
-    description = "Allow traffic from NGINX"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "-1"
-    cidr_blocks = var.cidr_block
-  }
 
   egress {
-    from_port   = var.from_port[2]
-    to_port     = var.to_port[2]
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = var.cidr_block
   }
@@ -45,9 +39,9 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Allow HTTP traffic"
-    from_port   = var.from_port[0]
-    to_port     = var.to_port[0]
+    description = "Allow traffic from ALB on 8080"
+    from_port   = 8080
+    to_port     = 8080
     protocol    = var.protocol
     security_groups = [aws_security_group.alb_sg.id]
   }
